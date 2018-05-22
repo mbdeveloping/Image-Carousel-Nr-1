d97fc5b7271c8c0cfce47066aada23d6b5d083c1
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-05-22 11:23:07 +0100

    Auto indicator

--- script/main-js.js
+++ script/main-js.js
@@ -10,11 +10,17 @@ $(document).ready(function() {
   function next() {
     let $prevtSlide = $allSlides.filter('.active-slide');
     let $currentSlide = $prevtSlide.next();
+    let $prevIndicator = $indicators.filter('.active-indicator');
+    let $currentIndicator = $prevIndicator.next();
+
+    $indicators.removeClass('active-indicator');
+    $currentIndicator.addClass('active-indicator');
     $allSlides.removeClass('active-slide');
     $currentSlide.addClass('active-slide');
     index++;
     if (index > 3) {
       $(".slide1").addClass('active-slide');
+      $(".img1-indicator").addClass('active-indicator');
       index = 1;
     }
   }
@@ -22,11 +28,17 @@ $(document).ready(function() {
   function prev() {
     let $prevtSlide = $allSlides.filter('.active-slide');
     let $currentSlide = $prevtSlide.prev();
+    let $prevIndicator = $indicators.filter('.active-indicator');
+    let $currentIndicator = $prevIndicator.prev();
+
+    $indicators.removeClass('active-indicator');
+    $currentIndicator.addClass('active-indicator');
     $allSlides.removeClass('active-slide');
     $currentSlide.addClass('active-slide');
     index--;
     if (index < 1) {
       $(".slide3").addClass('active-slide');
+      $(".img3-indicator").addClass('active-indicator');
       index = 3;
     }
   }
@@ -50,6 +62,10 @@ $(document).ready(function() {
   $indicators.on('click', function(e) {
     let target = e.target || e.srcElement;
     let targetNumber = target.className.match(/img(\d+)/)[1];
+
+    $indicators.removeClass('active-indicator');
+    $(this).addClass('active-indicator');
+
     $allSlides.removeClass('active-slide');
     $allSlides.eq(targetNumber-1).addClass('active-slide');
     index = targetNumber;
